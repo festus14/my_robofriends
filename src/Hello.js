b4b3f4b1318cda78eb889b942f68f8b0859cacee
Festus Omole <festus.omole@intranet.com>
2019-07-08 18:35:06 +0100

    Initial Commit

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Hello.css'
+
+class Hello extends Component{
+    render(){
+        return(
+            <div className='fi tc'>
+                <h1>Hello World!</h1>
+                <p>Welcome to react</p>
+            </div>
+        )
+    }
+}
+
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello from './Hello.js';
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'tachyons';
+ReactDOM.render(<Hello />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
